5eeb846126318a86a43461b2c32d97b48fa44104
Minsoo Kang <alstn38@naver.com>
2023-10-18 16:37:36 +0900

    init

---
+++ KangMinSoo-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KangMinSoo-assignment
+//
+//  Created by 강민수 on 10/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
